e80c3d66e902e20deb87102631f2b9c21650413e
HosseinDehghani <h.dehqani@gmail.com>
2016-10-20 19:45:38 -0500

    Added mising file

---
+++ path-generation/OnePointSuturing/Suturing.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Media3D;
+using System.Windows.Forms;
+
+namespace path_generation.OnePointSuturing
+{
+    public class Suturing
+    {
+        //public Joints joints;
+        public Needle needle; // the trajectory will initialize the needle, then will get updated based on new needle tip.
+        double t = 0;
+        public int state;
+        public Suturing()
+        {
+            state = 0;
+        }
+        public void START_SUTURING()
+        {
+            Console.Write("\nSuturing satrted.\n");
+            needle = new Needle();
+            t = 0;
+            state = 1; // state initialization: state 1 indicates entry, state 2 exit and state 3 the suturing
+        }
+        public void SELECT_ENTRY(Joints joints)
+        {
+            /*
+            needle.kinematics.joint.UpperBevel = joints.UpperBevel;
+            needle.kinematics.joint.LowerBevel = joints.LowerBevel;
+            needle.kinematics.joint.Elbow = joints.Elbow;
+            needle.kinematics.joint.twist = joints.twist;
+            needle.update_needle();* */
+            state++;
+             
+            state++;
+        }
+        public void SELECT_ENTRY(double x, double y, double z)
+        {
+            Console.Write("\nState 1: ENTRY POINT selected\n");
+            Vector3D entry_point = new Vector3D(x, y, z);
+            //Vector3D entry_point = new Vector3D(-30, 0, 130);
+            Print.print_vector(entry_point);
+            state++;
+        }
+        public void SELECT_EXIT(double x, double y, double z)
+        {
+            Console.Write("\nState 2 EXIT POINT selected\n");
+            Vector3D exit_point = new Vector3D(x, y, z);
+            //Vector3D exit_point = new Vector3D(-25, 0, 130);
+            Print.print_vector(exit_point);
+            state++;
+        }
+        public bool INITIALIZE_SUTURING()
+        {//-60,-60,80,0
+            
+            needle.kinematics.joint.UpperBevel = 40;
+            needle.kinematics.joint.LowerBevel = 30;
+            needle.kinematics.joint.Elbow = -70;
+            needle.kinematics.joint.twist = 0;
+            needle.update_needle();
+            
+            state++;
+            return true;
+        }
+        public void PRE_SUTURING()
+        {
+        }
+        public bool DO_SUTURING()
+        {
+            Optimizer optimizer = new Optimizer();
+            optimizer.T_taget = needle.moved_head;
+
+            Joints optimized_joints=optimizer.minimize_error();
+            needle.kinematics.joint.UpperBevel = optimized_joints.UpperBevel;
+            needle.kinematics.joint.LowerBevel = optimized_joints.LowerBevel;
+            needle.kinematics.joint.Elbow = optimized_joints.Elbow;
+            needle.kinematics.joint.twist = optimized_joints.twist;
+            needle.update_needle();
+
+            if (t >= 2 * Math.PI)
+            {
+                END_SUTURING();
+                Console.Write("\nAutomatically ended\n");
+                return true;
+            }
+            return false;
+        }
+
+        public void END_SUTURING()
+        {
+            Console.Write("\nSuturing ended.\n");
+            t = 0;
+            state = 0;
+        }
+        private double twist_correction(double t)
+        {
+            return (-t * 180 / Math.PI);
+        }
+    }
+}
---
+++ path-generation/Trajectory0.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathWorks.MATLAB.NET.Arrays;
+using MathWorks.MATLAB.NET.Utility;
+using minimizerNamespace;
+using System.Windows.Media.Media3D;
+
+namespace path_generation
+{
+    public class Trajectory0
+    {
+        public double needle_radius = 14; // radius of the needle
+        public Vector3D entry_point;
+        public Vector3D exit_point;
+        public Vector3D center;
+        public Vector3D normal;
+        public int number_of_points = 20;
+        public Coordinate local_coordinate;
+        public Coordinate twisted_local_coordinate;
+        public double incr { get; private set; }
+
+        public Vector3D needle_tip_position;
+        public double needle_tip_twist { get; private set; }
+
+        public Trajectory0()
+        {
+        }
+        public Trajectory0(Vector3D entry_point, Vector3D exit_point)
+        {
+            this.entry_point = entry_point;
+            this.exit_point = exit_point;
+            create();
+
+        }
+        public void create() // enrty and exit point must be given beforehand
+        {
+            incr = Math.PI / number_of_points;
+            local_coordinate.origin = new Vector3D();
+            local_coordinate.e_x = new Vector3D();
+            local_coordinate.e_y = new Vector3D();
+            local_coordinate.e_z = new Vector3D();
+            twisted_local_coordinate.origin = new Vector3D();
+            twisted_local_coordinate.e_x = new Vector3D();
+            twisted_local_coordinate.e_y = new Vector3D();
+            twisted_local_coordinate.e_z = new Vector3D();
+            twisted_local_coordinate.e_x = new Vector3D(); //e: unit vector
+            twisted_local_coordinate.e_x = exit_point - entry_point;
+            local_coordinate.e_x = new Vector3D(twisted_local_coordinate.e_x.X, 0, twisted_local_coordinate.e_x.Z);
+            local_coordinate.e_x = local_coordinate.e_x / local_coordinate.e_x.Length;
+            local_coordinate.e_y = new Vector3D(0, 1, 0);
+            local_coordinate.e_z = Vector3D.CrossProduct(local_coordinate.e_y, twisted_local_coordinate.e_x);
+            local_coordinate.e_z = local_coordinate.e_z / local_coordinate.e_z.Length;
+            twisted_local_coordinate.e_y = Vector3D.CrossProduct(local_coordinate.e_z, twisted_local_coordinate.e_x);
+            Vector3D offset = new Vector3D();
+            offset = Math.Sqrt(needle_radius * needle_radius - .25 * twisted_local_coordinate.e_x.Length * twisted_local_coordinate.e_x.Length) * twisted_local_coordinate.e_y / twisted_local_coordinate.e_y.Length;
+            local_coordinate.origin = new Vector3D(0.5 * (entry_point.X + exit_point.X), 0.5 * (entry_point.Y + exit_point.Y), 0.5 * (entry_point.Z + exit_point.Z));
+            local_coordinate.origin = local_coordinate.origin - offset;
+            twisted_local_coordinate.origin = local_coordinate.origin;
+            twisted_local_coordinate.e_z = local_coordinate.e_z;
+
+            needle_tip_position = entry_point;
+            needle_tip_twist = Math.Acos(Vector3D.DotProduct((needle_tip_position - local_coordinate.origin), local_coordinate.e_x) / ((needle_tip_position - local_coordinate.origin).Length * local_coordinate.e_x.Length));
+            needle_tip_twist = Math.PI - needle_tip_twist;
+
+            center = new Vector3D();
+            center = local_coordinate.origin;
+            normal = new Vector3D();
+            normal = local_coordinate.e_z;
+            Console.Write("\nreal entry\n");
+            Print0.print_vector(entry_point);
+            Console.Write("\nreal exit\n");
+            Print0.print_vector(exit_point);
+            Console.Write("\ncenter\n");
+            Print0.print_vector(center);
+
+        }
+        public Vector3D update_needle_tip_position()
+        {
+            Vector3D circle_normal = new Vector3D();
+            circle_normal = local_coordinate.e_z;
+            Vector3D circle_center = new Vector3D();
+            circle_center = local_coordinate.origin;
+            Vector3D temp = new Vector3D();
+            temp = needle_tip_position - circle_center;
+            Quaternion q_needle_tip_position = new Quaternion(temp.X, temp.Y, temp.Z, 0);
+            Quaternion q_circle_normal = new Quaternion(Math.Sin(-incr / 2) * circle_normal.X, Math.Sin(-incr / 2) * circle_normal.Y, Math.Sin(-incr / 2) * circle_normal.Z, Math.Cos(-incr / 2));
+            Quaternion q_circle_normal_conjugate = new Quaternion();
+            q_circle_normal_conjugate = q_circle_normal;
+            q_circle_normal_conjugate.Conjugate();
+            q_needle_tip_position = q_circle_normal * q_needle_tip_position * q_circle_normal_conjugate;
+            needle_tip_position.X = q_needle_tip_position.X + circle_center.X;
+            needle_tip_position.Y = q_needle_tip_position.Y + circle_center.Y;
+            needle_tip_position.Z = q_needle_tip_position.Z + circle_center.Z;
+            /*
+            needle_tip_position = rotateAboutAxis(normal, needle_tip_position - center, incr);
+            needle_tip_position = needle_tip_position + center;
+            */
+            return needle_tip_position;
+        }
+        private Vector3D rotateAboutAxis(Vector3D axis, Vector3D point, double angle)
+        {
+            Vector3D rotated = new Vector3D();
+            Matrix3D matrix = Matrix3D.Identity;
+            Quaternion q_axis = new Quaternion(Math.Sin(angle / 2) * axis.X, Math.Sin(angle / 2) * axis.Y, Math.Sin(angle / 2) * axis.Z, Math.Cos(angle / 2));
+            matrix.Rotate(q_axis);
+            rotated = matrix.Transform(point);
+
+            return rotated;
+        }
+        public double update_needle_tip_twist()
+        {
+            needle_tip_twist = needle_tip_twist + incr;
+            return needle_tip_twist;
+        }
+
+
+
+
+    }
+}
